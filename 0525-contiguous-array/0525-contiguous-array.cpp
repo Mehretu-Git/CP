ce6171e77fec7522449062257f8b70670578cefc
Abate <131160315+Mehretu-Git@users.noreply.github.com>
2026-01-02 17:49:39 +0300

    Time: 24 ms (53.82%), Space: 88.4 MB (26.07%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        for(int &bit: nums){
-            if(bit == 0) bit = -1; 
+        for (int& bit : nums) {
+            if (bit == 0)
+                bit = -1;
         }
         int running_sum{};
         int max_length{};
-        unordered_map<int,int>sum_to_index{{0,-1}};//sum was 0 before the array begins
-        for(int i{};i<nums.size();++i){
+        unordered_map<int, int> sum_to_index{
+            {0, -1}}; // sum was 0 before the array begins
+        for (int i{}; i < nums.size(); ++i) {
             running_sum += nums[i];
-            if(sum_to_index.count(running_sum)){//found a subarray whose sum is 0
+            if (sum_to_index.count(
+                    running_sum)) { // found a subarray whose sum is 0
                 int temp_length = i - sum_to_index[running_sum];
                 max_length = max(max_length, temp_length);
-            }
-            else
+            } else
                 sum_to_index[running_sum] = i;
         }
         return max_length;
