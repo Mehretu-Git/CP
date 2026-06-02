d562524dbacb5a8b92ee86891b2d8b268f036a55
Abate <131160315+Mehretu-Git@users.noreply.github.com>
2026-06-02 03:08:39 +0300

    Time: 0 ms (100%), Space: 14.2 MB (50.46%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.begin(),cost.end());
+        int min_cost = 0;
+        int max_cost = 0;
+        int discount = 0;
+        if(cost.size()==1) return cost[0];
+        else if(cost.size() == 2) return  cost[0] + cost[1];
+        else{
+            for(int i = cost.size() - 1; i>=0; i--){
+                max_cost += cost[i];
+            }
+            for(int i = cost.size() - 3; i>=0;){
+                discount += cost[i];
+                i -= 3;
+            }
+        }
+        return max_cost - discount;
+    }
+};
