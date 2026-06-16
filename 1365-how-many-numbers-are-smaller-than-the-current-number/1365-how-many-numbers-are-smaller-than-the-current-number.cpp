7d71139bf15098f231c75c0ea30c9a1c78d6527f
Abate <131160315+Mehretu-Git@users.noreply.github.com>
2026-06-16 07:04:27 +0300

    Time: 0 ms (100%), Space: 14.7 MB (9.77%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> ans;
 
         sort(nums.begin(),nums.end());
-        map<int,int> num_to_smaller;
+        unordered_map<int,int> num_to_smaller;
         for(int i{};i<nums.size();i++)
             if (num_to_smaller.count(nums[i]) == 0) 
                 num_to_smaller[nums[i]] = i;
