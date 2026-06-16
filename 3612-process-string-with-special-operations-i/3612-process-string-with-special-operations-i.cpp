fcd0fe9e6453e8cea1155da7e5ab1ecfa9122bfb
Abate <131160315+Mehretu-Git@users.noreply.github.com>
2026-06-16 03:56:51 +0300

    Time: 1 ms (83.71%), Space: 40.2 MB (17.8%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -1,26 +1,11 @@
 class Solution {
 public:
-    string reversed(string s){
-        if(s.size() <= 1) return s;
-        return reversed(s.substr(1)) + s[0];
-    }
-    string duplicated(string s){
-        string newS = s;
-        for(char c: s){
-            newS += c;
-        }
-        return newS;
-    }
-    string popped(string s){
-        if (s.empty()) return s; 
-        return s.substr(0, s.size() - 1);
-    }
     string processStr(string s) {
         string result = "";
         for(char c: s){
-            if(c=='*') result = popped(result);
-            else if(c=='#') result = duplicated(result);
-            else if(c =='%') result = reversed(result);
+            if(c=='*') result = result.substr(0, result.size() - 1);
+            else if(c=='#') result += result;
+            else if(c =='%') reverse(result.begin(),result.end());
             else result += c;
         }
         return result;
