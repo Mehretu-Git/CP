2e6ce7d34a2f44c5c6a84988aeaacc0f674d08cd
Meheretu.A <131160315+Mehretu-Git@users.noreply.github.com>
2026-07-15 16:23:45 +0300

    Time: 0 ms (100%), Space: 8.5 MB (85.02%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int sumOdd = n*n;
-        int sumEven = n*(n+1);
-        
-        cout<<sumOdd<<" "<<sumEven<<endl;
-        return gcd(sumOdd, sumEven);
+        return n;
     }
 };
