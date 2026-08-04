cf9b007710764ef557fa7d5298c247fbef94a01e
Meheretu.A <131160315+Mehretu-Git@users.noreply.github.com>
2026-08-04 17:29:32 +0300

    Time: 3 ms (49.33%), Space: 33.6 MB (28.39%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -5,12 +5,12 @@ public:
         int mn = nums[0];
         int mx = nums[size(nums) - 1];
         vector<int> ans;
-       for(int i = mn; i<mx; i++){
-            if((find(nums.begin(), nums.end(), i))!=nums.end()) continue;
-            else{
-                ans.push_back(i);
-            }
-       }
+
+        unordered_set<int> rNums(nums.begin(), nums.end());
+
+       for(int i = mn; i<=mx; i++)
+            if(!rNums.contains(i)) ans.push_back(i);
+       
        return ans;
     }
 };
